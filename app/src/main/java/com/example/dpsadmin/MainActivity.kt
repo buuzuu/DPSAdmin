7e4daef973fcbda50bd7f10bb3514d3875289643
Hritik <hritikgupta722@gmail.com>
2020-04-13 15:38:33 +0530

    Adding student registration

--- app/src/main/java/com/example/dpsadmin/MainActivity.kt
+++ app/src/main/java/com/example/dpsadmin/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.dpsadmin
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.Window
+import android.view.WindowManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,38 +15,52 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.dpsadmin.ui.dashboard.DashboardFragment
 import com.example.dpsadmin.ui.home.HomeFragment
 import com.example.dpsadmin.ui.notifications.NotificationsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var fragment: Fragment
-
+    lateinit var myWindow: Window
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        myWindow = this.window
         supportFragmentManager.beginTransaction().add(R.id.nav_host_fragment, HomeFragment())
             .commit()
 
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        navView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_home -> {
+        bubble.setNavigationChangeListener { _, position ->
+            when(position){
+                0 ->
                     fragment = HomeFragment()
-                    //return@setOnNavigationItemSelectedListener true
-                }
-                R.id.navigation_dashboard -> {
+                1 ->
                     fragment = DashboardFragment()
-                    //return@setOnNavigationItemSelectedListener true
-                }
-                R.id.navigation_notifications -> {
+                2 ->
                     fragment = NotificationsFragment()
-                    //return@setOnNavigationItemSelectedListener true
-                }
             }
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.nav_host_fragment, fragment)
-                .commit()
-            false
+            changeStatusColor(position)
+            supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment,fragment).commit()
         }
+    }
+
+    private fun changeStatusColor(pos:Int){
+        when (pos) {
+            0 -> {
+                myWindow.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+                myWindow.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                myWindow.statusBarColor = getColor(R.color.home)
+            }
+            1 -> {
+                myWindow.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+                myWindow.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                myWindow.statusBarColor = getColor(R.color.bill)
+            }
+            2 -> {
+                myWindow.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+                myWindow.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                myWindow.statusBarColor = getColor(R.color.salary)
+            }
+        }
+
 
     }
 }
---
+++ app/src/main/java/com/example/dpsadmin/StudentRegistration.kt
@@ -0,0 +1,12 @@
+package com.example.dpsadmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudentRegistration : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_registration)
+    }
+}
